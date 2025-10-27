b35802dd0a29368456c1063ee952e3d8ba0f7ad6
Kuroyukihime00 <abhivandyaaudryriana@gmail.com>
2025-10-27 08:20:44 +0700

    Update

--- js/grafika_lib.js
+++ js/grafika_lib.js
@@ -1,4 +1,18 @@
-import { clearCanvas, plotDot, drawLine, drawCircle } from "./grafika_lib.js";
+import {
+	gbr_titik,
+	lingkaran_polar,
+	ellipse_polar,
+	gbr_lingkaran,
+	dda_line,
+	polyline,
+	polygon,
+	floodFillNaive,
+	floodFillStack,
+	clearCanvas,
+	plotDot,
+	drawLine,
+	drawCircle,
+} from "./grafika_lib.js";
 
 export function render(
 	graph,
@@ -53,6 +67,146 @@ export function drawCircle(xc, yc, r, color) {
 		else {
 			y--;
 			p += 2 * (x - y) + 1;
+		}
+	}
+}
+
+export function gbr_titik(imageDataTemp, x, y, r, g, b) {
+	var index;
+	if (x > 0 && x < canvasKita.width && y > 0 && y < canvasKita.height) {
+		index = 4 * (x + y * canvasKita.width);
+		imageDataTemp.data[index] = r;
+		imageDataTemp.data[index + 1] = g;
+		imageDataTemp.data[index + 2] = b;
+		imageDataTemp.data[index + 3] = 255;
+	}
+}
+
+export function lingkaran_polar(imageDataTemp, xc, yc, radius, r, g, b) {
+	for (var theta = 0; theta < Math.PI * 2; theta += 0.01) {
+		x = xc + radius * Math.cos(theta);
+		y = yc + radius * Math.sin(theta);
+		gbr_titik(imageDataTemp, Math.ceil(x), Math.ceil(y), r, g, b);
+	}
+}
+
+export function ellipse_polar(
+	imageDataTemp,
+	xc,
+	yc,
+	radiusX,
+	radiusY,
+	r,
+	g,
+	b
+) {
+	for (var theta = 0; theta < Math.PI * 2; theta += 0.01) {
+		x = xc + radiusX * Math.cos(theta);
+		y = yc + radiusY * Math.sin(theta);
+		gbr_titik(imageDataTemp, Math.ceil(x), Math.ceil(y), r, g, b);
+	}
+}
+
+export function gbr_lingkaran(imageDataTemp, xc, yc, radius, r, g, b) {
+	for (var x = xc - radius; x < xc + radius; x++) {
+		var y = yc - Math.sqrt(Math.pow(radius, 2) - Math.pow(x - xc, 2));
+		gbr_titik(imageDataTemp, Math.ceil(x), Math.ceil(y), r, g, b);
+	}
+}
+
+export function dda_line(imageData, x1, y1, x2, y2, r, g, b) {
+	var dx = x2 - x1;
+	var dy = y2 - y1;
+	if (Math.abs(dx) > Math.abs(dy)) {
+		if (x2 > x1) {
+			var y = y1;
+			for (var x = x1; x < x2; x++) {
+				y = y + dy / Math.abs(dx);
+				gbr_titik(imageData, Math.ceil(x), Math.ceil(y), r, g, b);
+			}
+		} else {
+			var y = y1;
+			for (var x = x1; x >= x2; x--) {
+				y = y + dy / Math.abs(dx);
+				gbr_titik(imageData, Math.ceil(x), Math.ceil(y), r, g, b);
+			}
+		}
+	} else {
+		if (y2 > y1) {
+			var x = x1;
+			for (var y = y1; y < y2; y++) {
+				x = x + dx / Math.abs(dy);
+				gbr_titik(imageData, Math.ceil(x), Math.ceil(y), r, g, b);
+			}
+		} else {
+			var x = x1;
+			for (var y = y1; y > y2; y--) {
+				x = x + dx / Math.abs(dy);
+				gbr_titik(imageData, Math.ceil(x), Math.ceil(y), r, g, b);
+			}
+		}
+	}
+}
+
+export function polyline(imageData, point_array, r, g, b) {
+	var point = point_array[0];
+	for (var i = 1; i < point_array.length; i++) {
+		var point_2 = point_array[i];
+		dda_line(imageData, point.x, point.y, point_2.x, point_2.y, r, g, b);
+		point = point_2;
+	}
+}
+
+export function polygon(imageData, point_array, r, g, b) {
+	var point = point_array[0];
+	for (var i = 1; i < point_array.length; i++) {
+		var point_2 = point_array[i];
+		dda_line(imageData, point.x, point.y, point_2.x, point_2.y, r, g, b);
+		point = point_2;
+	}
+	dda_line(
+		imageData,
+		point_array[0].x,
+		point_array[0].y,
+		point.x,
+		point.y,
+		r,
+		g,
+		b
+	);
+}
+
+export function floodFillNaive(imageData, canvas, x, y, toFlood, color) {
+	var index = 4 * (x + y * canvas.width);
+	var r1 = imageData.data[index];
+	var g1 = imageData.data[index + 1];
+	var b1 = imageData.data[index + 2];
+	if (r1 == toFlood.r && g1 == toFlood.g && b1 == toFlood.b) {
+		imageData.data[index] = color.r;
+		imageData.data[index + 1] = color.g;
+		imageData.data[index + 2] = color.b;
+		imageData.data[index + 3] = 255;
+	}
+}
+
+export function floodFillStack(imageData, canvas, x, y, toFlood, color) {
+	var tumpukan = [];
+	tumpukan.push({ x: x, y: y });
+	while (tumpukan.length > 0) {
+		var titik_skrg = tumpukan.pop();
+		var index_skrg = 4 * (titik_skrg.x + titik_skrg.y * canvas.width);
+		var r1 = imageData.data[index_skrg];
+		var g1 = imageData.data[index_skrg + 1];
+		var b1 = imageData.data[index_skrg + 2];
+		if (r1 == toFlood.r && g1 == toFlood.g && b1 == toFlood.b) {
+			imageData.data[index_skrg] = color.r;
+			imageData.data[index_skrg + 1] = color.g;
+			imageData.data[index_skrg + 2] = color.b;
+			imageData.data[index_skrg + 3] = 255;
+			tumpukan.push({ x: titik_skrg.x + 1, y: titik_skrg.y });
+			tumpukan.push({ x: titik_skrg.x - 1, y: titik_skrg.y });
+			tumpukan.push({ x: titik_skrg.x, y: titik_skrg.y + 1 });
+			tumpukan.push({ x: titik_skrg.x, y: titik_skrg.y - 1 });
 		}
 	}
 }
--- js/transform.js
+++ js/transform.js
@@ -1,62 +1,73 @@
-function translasi(titik_lama, T){
-    var x_baru = titik_lama.x + T.x;
-    var y_baru = titik_lama.y + T.y;
+import {
+	translasi,
+	penskalaan,
+	rotasi,
+	rotasi_fp,
+	skala_fp,
+	rotasi_array,
+	skala_array,
+	translasi_array,
+} from "./transform.js";
 
-    return {x:x_baru, y:y_baru};
+export function translasi(titik_lama, T) {
+	var x_baru = titik_lama.x + T.x;
+	var y_baru = titik_lama.y + T.y;
+
+	return { x: x_baru, y: y_baru };
 }
 
-function penskalaan(titik_lama, S){
-    var x_baru = titik_lama.x * S.x;
-    var y_baru = titik_lama.y * S.y;
+export function penskalaan(titik_lama, S) {
+	var x_baru = titik_lama.x * S.x;
+	var y_baru = titik_lama.y * S.y;
 
-    return { x: x_baru, y: y_baru}
+	return { x: x_baru, y: y_baru };
 }
 
-function rotasi(titik_lama, sudut) {
-    var x_baru = titik_lama.x * Math.cos(sudut) - titik_lama.y * Math.sin(sudut);
-    var y_baru = titik_lama.x * Math.sin(sudut) + titik_lama.y * Math.cos(sudut);
-    return { x: x_baru, y: y_baru };
+export function rotasi(titik_lama, sudut) {
+	var x_baru = titik_lama.x * Math.cos(sudut) - titik_lama.y * Math.sin(sudut);
+	var y_baru = titik_lama.x * Math.sin(sudut) + titik_lama.y * Math.cos(sudut);
+	return { x: x_baru, y: y_baru };
 }
 
-function rotasi_fp(titik_lama, titik_putar, sudut){
-    var p1 = translasi(titik_lama, {x:-titik_putar.x, y:-titik_putar.y});
-    var p2 = rotasi(p1, sudut);
-    var p3 = translasi(p2, titik_putar);
-    
-    return p3;
+export function rotasi_fp(titik_lama, titik_putar, sudut) {
+	var p1 = translasi(titik_lama, { x: -titik_putar.x, y: -titik_putar.y });
+	var p2 = rotasi(p1, sudut);
+	var p3 = translasi(p2, titik_putar);
+
+	return p3;
 }
 
-function skala_fp(titik_lama, titik_putar, S){
-    var p1 = translasi(titik_lama, {x:-titik_putar.x, y:-titik_putar.y});
-    var p2 = penskalaan(p1, S);
-    var p3 = translasi(p2, titik_putar);
-    
-    return p3;
+export function skala_fp(titik_lama, titik_putar, S) {
+	var p1 = translasi(titik_lama, { x: -titik_putar.x, y: -titik_putar.y });
+	var p2 = penskalaan(p1, S);
+	var p3 = translasi(p2, titik_putar);
+
+	return p3;
 }
 
-function rotasi_array(array_titik, titik_pusat, sudut){
-    var array_hasil = [];
-    for (var i=0;i<array_titik.length;i++) {
-        var temp = rotasi_fp(array_titik[i], titik_pusat, sudut);
-        array_hasil.push(temp);
-    }
-    return array_hasil;
+export function rotasi_array(array_titik, titik_pusat, sudut) {
+	var array_hasil = [];
+	for (var i = 0; i < array_titik.length; i++) {
+		var temp = rotasi_fp(array_titik[i], titik_pusat, sudut);
+		array_hasil.push(temp);
+	}
+	return array_hasil;
 }
 
-function skala_array(array_titik, titik_pusat, S){
-    var array_hasil = [];
-    for (var i=0;i<array_titik.length;i++) {
-        var temp = skala_fp(array_titik[i], titik_pusat, S);
-        array_hasil.push(temp);
-    }
-    return array_hasil;
+export function skala_array(array_titik, titik_pusat, S) {
+	var array_hasil = [];
+	for (var i = 0; i < array_titik.length; i++) {
+		var temp = skala_fp(array_titik[i], titik_pusat, S);
+		array_hasil.push(temp);
+	}
+	return array_hasil;
 }
 
-function translasi_array(array_titik, T){
-    var array_hasil = [];
-    for (var i=0;i<array_titik.length;i++) {
-        var temp = translasi(array_titik[i], T);
-        array_hasil.push(temp);
-    }
-    return array_hasil;
+export function translasi_array(array_titik, T) {
+	var array_hasil = [];
+	for (var i = 0; i < array_titik.length; i++) {
+		var temp = translasi(array_titik[i], T);
+		array_hasil.push(temp);
+	}
+	return array_hasil;
 }
